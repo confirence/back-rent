4e768bae274ee6626c773c6fff38e87d98fc534d
SayFu11a <sgishlakaev@mail.ru>
2024-03-04 22:16:00 +0300

    коммит исправил контроллер, добавил модель бронирования, сделал сохранение брони на сервере

--- controllers/BookingController.js
+++ controllers/BookingController.js
@@ -1,10 +1,57 @@
-export const BookingController = {
-   handleBooking: (req, res) => {
-      const bookingData = req.body;
-      // Ваш код обработки данных о бронировании
-      console.log('Данные о бронировании с клиента:', bookingData);
+import BookingModel from '../models/Booking.js';
 
-      // Отправка ответа об успешном выполнении
-      res.json({ message: 'Бронирование успешно обработано на сервере' });
+export const BookingController = {
+   handleBooking: async (req, res) => {
+      const bookingData = req.body;
+
+      try {
+         // Сохранение данных о бронировании в базе данных
+         const newBooking = await BookingModel.create(bookingData);
+
+         console.log('Данные о бронировании сохранены:', newBooking);
+
+         // Отправка ответа об успешном выполнении
+         res.json({ message: 'Бронирование успешно обработано на сервере' });
+      } catch (err) {
+         console.error(err);
+         res.status(500).json({
+            message: 'Ошибка при сохранении данных о бронировании',
+         });
+      }
    },
 };
+
+// import BookingModel from '../models/Booking.js';
+
+// export const BookingController = {
+//    handleBooking: async (req, res) => {
+//       const bookingData = req.body;
+
+//       try {
+//          // Сохранение данных о бронировании в базе данных
+//          const newBooking = await BookingModel.create(bookingData);
+
+//          console.log('Данные о бронировании сохранены:', newBooking);
+
+//          // Отправка ответа об успешном выполнении
+//          res.json({ message: 'Бронирование успешно обработано на сервере' });
+//       } catch (err) {
+//          console.error(err);
+//          res.status(500).json({
+//             message: 'Ошибка при сохранении данных о бронировании',
+//          });
+//       }
+//    },
+
+//    getAllBookings: async (req, res) => {
+//       try {
+//          const bookings = await BookingModel.find();
+//          res.json(bookings);
+//       } catch (err) {
+//          console.error(err);
+//          res.status(500).json({
+//             message: 'Ошибка при получении данных о бронированиях',
+//          });
+//       }
+//    },
+// };
---
+++ models/booking.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const BookingSchema = new mongoose.Schema(
+   {
+      checkInDate: {
+         type: Date,
+         required: true,
+      },
+      checkOutDate: {
+         type: Date,
+         required: true,
+      },
+      contactInfo: {
+         fullName: {
+            type: String,
+            required: true,
+         },
+         email: {
+            type: String,
+            required: true,
+         },
+         phoneNumber: {
+            type: String,
+            required: true,
+         },
+      },
+      guests: [
+         {
+            fullName: {
+               type: String,
+               required: true,
+            },
+         },
+      ],
+   },
+   {
+      timestamps: true,
+   },
+);
+
+const BookingModel = mongoose.model('Booking', BookingSchema);
+
+export default BookingModel;
